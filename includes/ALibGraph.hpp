236a765f5549fc7b2756b191df63744841fe9531
tran_0 <david.tran@epitech.eu>
2015-03-26 18:35:16 +0100

    push Abstract

--- includes/ALibGraph.hpp
+++ includes/ALibGraph.hpp
@@ -5,7 +5,7 @@
 // Login   <tran_0@epitech.net>
 // 
 // Started on  Wed Mar 25 14:15:03 2015 David Tran
-// Last update Thu Mar 26 18:13:51 2015 David Tran
+// Last update Thu Mar 26 18:25:27 2015 David Tran
 //
 
 #ifndef ALIBGRAPH_HPP_
@@ -20,9 +20,9 @@ public:
   virtual	~ALibGraph(){}
   ALibGraph(int x, int y) : maxX(x), maxY(y){}
   bool		Init() = 0;
-  bool		DrawMap(Map const &map) = 0;
+  bool		DrawMap(MAP const &map) = 0;
   void		Destroy() = 0;
-  bool		DrawQuadra() = 0;
+  bool		DrawQuadra(MAP const &map) = 0;
   bool		DrawHUD() = 0;
   char		HandleEvent() = 0;
   int		getMaxX(){return (maxX);}
