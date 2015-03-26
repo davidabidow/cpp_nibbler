c26a7bcfda6c25a69f4dd268acc8c814d56543ee
gregoi_j <jean-baptiste.gregoire@epitech.eu>
2015-03-26 23:31:58 +0100

    Add defines

--- includes/ALibGraph.hpp
+++ includes/ALibGraph.hpp
@@ -14,19 +14,22 @@
 # include "nibbler.hpp"
 # include "Map.hpp"
 
+# define WIN_WIDTH  1280
+# define WIN_HEIGHT 720
+
 class		ALibGraph
 {
 public:
   virtual	~ALibGraph(){}
   ALibGraph(int x, int y) : maxX(x), maxY(y) {}
-  bool		Init() = 0;
-  bool		DrawMap(Map const &map) = 0;
-  void		Destroy() = 0;
-  bool		DrawQuadra(Map const &map) = 0;
-  bool		DrawHUD() = 0;
-  char		HandleEvent() = 0;
-  int		getMaxX() const {return (maxX);}
-  int		getMaxY() const {return (maxY);}
+  virtual bool		Init() = 0;
+  virtual bool		DrawMap(Map const &map) = 0;
+  virtual void		Destroy() = 0;
+  virtual bool		DrawQuadra(Map const &map) = 0;
+  virtual bool		DrawHUD() = 0;
+  virtual char		HandleEvent() = 0;
+  int             getMaxX() const {return (maxX);}
+  int             getMaxY() const {return (maxY);}
 private:
   int			maxX;
   int			maxY;
