d0cf6c5047c8467ee78eecab9a35a1562bdd3ce5
tran_0 <david.tran@epitech.eu>
2015-03-26 18:39:10 +0100

    constness

--- includes/ALibGraph.hpp
+++ includes/ALibGraph.hpp
@@ -5,7 +5,7 @@
 // Login   <tran_0@epitech.net>
 // 
 // Started on  Wed Mar 25 14:15:03 2015 David Tran
-// Last update Thu Mar 26 18:25:27 2015 David Tran
+// Last update Thu Mar 26 18:38:57 2015 David Tran
 //
 
 #ifndef ALIBGRAPH_HPP_
@@ -25,8 +25,8 @@ public:
   bool		DrawQuadra(MAP const &map) = 0;
   bool		DrawHUD() = 0;
   char		HandleEvent() = 0;
-  int		getMaxX(){return (maxX);}
-  int		getMaxY(){return (maxY);}
+  int		getMaxX() const {return (maxX);}
+  int		getMaxY() const {return (maxY);}
 private:
   int			maxX;
   int			maxY;
