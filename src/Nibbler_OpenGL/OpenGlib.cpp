ac528df60f4112d807fd5ee32d8811e67e8a1962
gregoi_j <jean-baptiste.gregoire@epitech.eu>
2015-03-30 14:49:54 +0200

    Add openGL lib

--- src/Nibbler_OpenGL/OpenGlib.cpp
+++ src/Nibbler_OpenGL/OpenGlib.cpp
@@ -5,7 +5,7 @@
 // Login   <gregoi_j@epitech.net>
 //
 // Started on  Wed Mar  25 18:29:42 2015 Jean-Baptiste Grégoire
-// Last update Fri Mar 27 02:00:57 2015 Jean-Baptiste Grégoire
+// Last update Mon Mar 30 14:49:18 2015 Jean-Baptiste Grégoire
 //
 
 #include "OpenGlib.hpp"
@@ -19,24 +19,11 @@ bool		OpenGlib::Init(int x, int y, int *argc, char **argv)
 {
   maxX = x;
   maxY = y;
-  glutInit(argc, argv);
-  glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-  glutInitWindowPosition(100, 100);
-  glutInitWindowSize(1280, 720);
-  if ((screen = glutCreateWindow("Nibbler -OpenGL-")) == 0)
-    throw ;
-
-  glutDisplayFunc(displayMap);
-  glutIdleFunc(displayMap);
-  glutKeyboardFunc(manageKeys);
-  glutSpecialFunc(manageSpecialKeys);
-
+  window.create(sf::VideoMode(WIN_WIDTH, WIN_HEIGHT), "Nibbler -OpenGL-");
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   gluPerspective(WIN_FOVY, static_cast<double>(x / y), NEAR, FAR);
-
-  //glEnable(GLUT_DEPTH_TEST);
-
+  glEnable(GLUT_DEPTH_TEST);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
   gluLookAt(50, 200, 200,
@@ -45,37 +32,8 @@ bool		OpenGlib::Init(int x, int y, int *argc, char **argv)
   return (true);
 }
 
-void		manageKeys(unsigned char key, UNUSED(int x), UNUSED(int y))
+bool		OpenGlib::DrawMap(Map const &map)
 {
-  switch(key)
-    {
-    case ESC_KEY: //leak memory baaaaah ...
-      exit(0);
-      break;
-    default:
-      break;
-    }
-}
-
-void		manageSpecialKeys(int key, UNUSED(int x), UNUSED(int y))
-{
-  switch (key)
-    {
-    case GLUT_KEY_LEFT:
-      turnLeft();
-      break;
-    case GLUT_KEY_RIGHT:
-      turnRight();
-      break;
-    default:
-      break;
-    }
-}
-
-void    	displayMap(void)
-{
-  Map 	map = Map::getInstance();
-
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
@@ -87,17 +45,12 @@ void    	displayMap(void)
   glVertex3d(-1 * (map.getMaxX() / 2), 0, map.getMaxY() / 2);
   glEnd();
   glFlush();
-}
-
-bool		OpenGlib::DrawMap(__attribute__((unused))Map const &map)
-{
-  displayMap();
   return (true);
 }
 
 void		OpenGlib::Destroy()
 {
-  glutDestroyWindow(screen);
+  window.close();
 }
 
 bool		OpenGlib::DrawQuadra()
